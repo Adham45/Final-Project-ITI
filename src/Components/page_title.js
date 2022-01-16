39b47020716cfe67bea5af4fff3aabdacc6b6548
691997 <95089880+691997@users.noreply.github.com>
2022-01-17 00:25:37 +0200

    Work in About Section

--- src/Components/page_title.js
+++ src/Components/page_title.js
@@ -1,7 +1,7 @@
 
 export default function Page_title (props){
     return (
-        <div className="page_title">
+        <div className="page_title mb-5">
             <div className="page_title_info">
                 <h2> {props.title} </h2>
                 <p> {props.description} </p>
