348d390c463a477ba31724cdca0dd5d1017fcad3
Ahmed Nabil <94ahmed.nabil@gmail.com>
2022-01-17 18:14:52 +0200

    my forms

---
+++ src/Components/forms/login.js
@@ -0,0 +1,99 @@
+
+import {useState} from "react";
+import Mail from "./email";
+import './form.css'
+
+
+
+
+const Login = () => {
+
+	const [pass, setPass] = useState("");
+	const [passError, setPassError] = useState(null);
+
+	const passValidation = (e) => {
+		console.log(e.target.value)
+
+		if(e.target.name === "userpass") {
+			setPass(e.target.value)
+      if(e.target.value===""){
+        setPassError(null);
+      }else if(e.target.value.length>=8 && e.target.value.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$")){
+        setPassError("");
+
+      }else{
+        setPassError("Please enter a vaild password format");
+
+      }
+
+}
+
+}
+
+  const showPass = () => {
+  const passInput= document.getElementById("passId")
+
+  if(passInput.type === "password") {
+    return passInput.type = "text"
+    
+  }else{
+    return passInput.type = "password"
+  }
+}
+
+    return (
+      
+  <section className="wholepage">
+      
+  <div className="container">
+  <div className="card loginform">
+  
+  <div className="card-body">
+    <h1 className="card-title text-center">Login</h1>
+  <div className="card-text">
+    <form>
+  <Mail />
+
+  <div className="mb-3 form-group">
+    <label for="passInput ">Password</label>
+    <input type="password" className="form-control"
+     id="passId"
+     required
+     value={pass}
+     name="userpass"
+     onChange={(e) => passValidation(e)}/>
+     <small> {passError}</small>
+     </div>
+
+     <div className="mb-3 form-group checkbox">
+     <input type="checkbox" onClick={()=>showPass()}/>
+     <label>show Password</label>
+     </div>
+  <button type="submit " className="btn btn-primary form-control">Login</button>
+
+  <div className="registermsg">
+    Don't have an account? <a href="/register" className="text-decoration-none">Create one</a>
+  </div>
+    </form>
+    </div>
+    </div>
+
+    </div>
+    </div>
+
+    </section>
+
+
+
+
+
+
+
+
+    
+    );
+  }
+
+  
+  export default Login;
+  
