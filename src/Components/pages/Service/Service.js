1d73d359d113c0addc92278705e077b94bcf6de1
ahmedewaidar22 <ahmedewaidar22@gmail.com>
2022-01-29 17:55:06 +0200

    Service Translation

--- src/Components/pages/Service/Service.js
+++ src/Components/pages/Service/Service.js
@@ -3,10 +3,37 @@ import Footer from "../Footer/Footer";
 import PageTitle from "../../page_title"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Service.css';
+import {Langcontext} from '../../../App'
+ import {useContext}from "react";
 const Services = () => {
+    const Arabic={
+        Services:" خدمات",
+        service_home:"القائمه الرئيسيه /خدمات",
+        FirstDivaccordion:"عملاء المنزل",
+        firstaccordionbody:"تختلف سياسة جمع القمامة وإعادة التدوير لدينا بناءً على معايير محددة. إذا كنت تعيش في شقة سكنية فإنه  يمكنك تحديد الكمية ومعرفة السعر قبل أن يأتي مندوبنا إليك و تكون الزيارة الاسبوعية مرة واحدة فقط فى الاسبوع (يوم الخميس)  و و يمكنك التعديل على البيانات و الكمية  التى لديك كما تريد حتى يوم الاربعاء حيث انه فى ذلك اليوم يتم اعتماد البيانات التى تم ادخالها  و يتم طباعة كشف  بالكمية و السعر اللذان تم اعتمادهم و اعطائهم للمندوب و الذى يتعامل مع العميل طبقا لما هو مسجل عنده فى الكشف :",
+        SecDivaccordion:"صاحب محل تجارى",
+        Secaccordionbody:" اذا كنت صاحب محل تجارى فلديك الخيار ان يمر عليك المندوب بشكل يومى او اسبوعى او شهرى حسب الكمية التى لديك حيث   ياتى المندوب فى اليوم التالى من تسجيل البيانات فى حالة اذا اخترت ان يمر عليك بشكل يومى امال اذا اخترت ان يمر عليك بشكل اسبوعى فسيمر عليك مرة واحدة  فى الاسبوع حسب المنطقة التى تنتمى اليها ",
+        ThirdDivaccordion:"العمال",
+        Thirdaccordionbody:"اذا كنت تعمل فى جمع القمامة و سواء كانت بلاستيك او الومنيوم او كرتون فإننا نستطيع ان نوفر لك دخل ثابت من خلال ان تعمل  معنا فى جمع هذة المواد و تستطيع ان تعرف معلومات اكتر عن طريق سيكشن العاملين"
+        }
+        const English={
+            Services:" Services",
+            service_home:"Home/Services",
+          FirstDivaccordion:"From Home",
+          firstaccordionbody:"Our garbage collection and recycling policy vary based on specific criteria. If you live in an apartment then it You can specify the quantity and know the price before our representative comes to you. The weekly visit is only once a week (on Thursday) and And you can modify the data and quantity that you have as you want until Wednesday, as on that day the data entered is approved  A statement is printed with the quantity and price that was approved and given to the representative, who deals with the customer according to what is registered with him in the statement.",
+          SecDivaccordion:"Shop Owner",
+          Secaccordionbody:"If you are the owner of a commercial store, you have the option to have the representative pass you on a daily, weekly, or monthly basis, according to a large number in the field. I stop you on a daily basis quickly on you only once. According to the region you belong to",
+          ThirdDivaccordion:" Workers",
+          Thirdaccordionbody:"If you work in collecting garbage, whether it is plastic, aluminum or cardboard, we can provide you with a fixed income by working with us in collecting these materials, and you can learn more information through the worker's section "
+          }
+
+          const{langcont,Setlangcontext} = useContext(Langcontext);
+          console.log(langcont,"langContext")
+          const translation=langcont==="ENGLISH"?English:Arabic;
+
     return (
         <div >
-            <PageTitle title="Service" description="Home / Service"/>
+            <PageTitle title={translation.Services} description={translation.service_home}/>
             <div className='container'>
                 <div className='row '>
                     <div className='col-lg-6 col-md-6 col-sm-12'>
@@ -28,21 +55,15 @@ const Services = () => {
                                         data-bs-target="#panelsStayOpen-collapseOne"
                                         aria-expanded="true"
                                         aria-controls="panelsStayOpen-collapseOne">
-                                        Accordion Item #1
-                                    </button>
+                                                {translation.FirstDivaccordion}
+                                        </button>
                                 </h2>
                                 <div
                                     id="panelsStayOpen-collapseOne"
                                     className="accordion-collapse collapse show"
                                     aria-labelledby="panelsStayOpen-headingOne">
                                     <div className="accordion-body">
-                                        <strong>This is the first item's accordion body.</strong>
-                                        It is shown by default, until the collapse plugin adds the appropriate classes
-                                        that we use to style each element. These classes control the overall appearance,
-                                        as well as the showing and hiding via CSS transitions. You can modify any of
-                                        this with custom CSS or overriding our default variables. It's also worth noting
-                                        that just about any HTML can go within the
-                                        <code>.accordion-body</code>, though the transition does limit overflow.
+                                       {translation.firstaccordionbody}
                                     </div>
                                 </div>
                             </div>
@@ -55,7 +76,7 @@ const Services = () => {
                                         data-bs-target="#panelsStayOpen-collapseTwo"
                                         aria-expanded="false"
                                         aria-controls="panelsStayOpen-collapseTwo">
-                                        Accordion Item #2
+                                        {translation.SecDivaccordion}
                                     </button>
                                 </h2>
                                 <div
@@ -63,13 +84,7 @@ const Services = () => {
                                     className="accordion-collapse collapse"
                                     aria-labelledby="panelsStayOpen-headingTwo">
                                     <div className="accordion-body">
-                                        <strong>This is the second item's accordion body.</strong>
-                                        It is hidden by default, until the collapse plugin adds the appropriate classes
-                                        that we use to style each element. These classes control the overall appearance,
-                                        as well as the showing and hiding via CSS transitions. You can modify any of
-                                        this with custom CSS or overriding our default variables. It's also worth noting
-                                        that just about any HTML can go within the
-                                        <code>.accordion-body</code>, though the transition does limit overflow.
+                                       {translation.firstaccordionbody}
                                     </div>
                                 </div>
                             </div>
@@ -82,7 +97,7 @@ const Services = () => {
                                         data-bs-target="#panelsStayOpen-collapseThree"
                                         aria-expanded="false"
                                         aria-controls="panelsStayOpen-collapseThree">
-                                        Accordion Item #3
+                                        {translation.ThirdDivaccordion}
                                     </button>
                                 </h2>
                                 <div
@@ -90,13 +105,7 @@ const Services = () => {
                                     className="accordion-collapse collapse"
                                     aria-labelledby="panelsStayOpen-headingThree">
                                     <div className="accordion-body">
-                                        <strong>This is the third item's accordion body.</strong>
-                                        It is hidden by default, until the collapse plugin adds the appropriate classes
-                                        that we use to style each element. These classes control the overall appearance,
-                                        as well as the showing and hiding via CSS transitions. You can modify any of
-                                        this with custom CSS or overriding our default variables. It's also worth noting
-                                        that just about any HTML can go within the
-                                        <code>.accordion-body</code>, though the transition does limit overflow.
+                                       {translation.Thirdaccordionbody}
                                     </div>
                                 </div>
                             </div>
@@ -121,9 +130,10 @@ const Services = () => {
                                         className='imgsize'/>
                                 </div>
                                 <div className="flip-card-back">
-                                    <h1>John Doe</h1>
-                                    <p>Architect Engineer</p>
-                                    <p>We love that guy</p>
+                                    <h1>{translation.FirstDivaccordion}</h1>
+                                    <hr/>
+
+                                    <p>{translation.firstaccordionbody}</p>
                                 </div>
                             </div>
                         </div>
@@ -143,9 +153,11 @@ const Services = () => {
 
                                 </div>
                                 <div className="flip-card-back">
-                                    <h1>John Doe</h1>
-                                    <p>Architect Engineer</p>
-                                    <p>We love that guy</p>
+                                    <h1>{translation.SecDivaccordion}</h1>
+                                    <hr/>
+
+                                    <p>{translation.Secaccordionbody}</p>
+                                  
                                 </div>
                             </div>
                         </div>
@@ -164,9 +176,10 @@ const Services = () => {
 
                                 </div>
                                 <div className="flip-card-back">
-                                    <h1>John Doe</h1>
-                                    <p>Architect Engineer</p>
-                                    <p>We love that guy</p>
+                                    <h1>{translation.ThirdDivaccordion}</h1>
+                                    <hr/>
+                                    <p>{translation.Thirdaccordionbody}</p>
+                                    
                                 </div>
                             </div>
                         </div>
