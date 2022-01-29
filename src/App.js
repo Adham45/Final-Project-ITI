289f10bc89cfb39543fe9879de39656ebf6e79be
ahmedewaidar22 <ahmedewaidar22@gmail.com>
2022-01-29 16:45:47 +0200

    context test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch
-} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import { useState } from 'react';
+
 
 import About from './Components/pages/About/About';
 import Contact from './Components/pages/Contact/Contact';
@@ -13,33 +10,41 @@ import Home from './Components/pages/Home/Home';
 import Services from './Components/pages/Service/Service';
 import Testimonial from './Components/pages/Testimonial/Testimonial';
 import Navbar from './Components/NavBar';
+// Transalation context
+export const Langcontext = React.createContext()
 
 const App = () => {
-  return (
-   <Router>
-     <main>
-    <Navbar/>
-      <Switch>
-        <Route path="/" exact>
-          <Home/>
-        </Route>
-        <Route path="/about" exact>
-          <About/>
-        </Route>
-        <Route path="/service" exact>
-          <Services/>
-        </Route>
-        <Route path="/testimonial" exact>
-          <Testimonial/>
-        </Route>
-        <Route path="/contact" exact>
-          <Contact/>
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </main>
-   </Router>
-  );
+    const [langcont, Setlangcontext] = useState("ENGLISH")
+    console.log(langcont, "langcont app")
+    return (
+        <div dir={langcont === "ENGLISH"? "ltr": "rtl"}>
+              <Router>
+              <Langcontext.Provider value={{langcont,Setlangcontext}}>
+                <main>
+                    <Navbar/>
+                    <Switch>
+                        <Route path="/" exact="exact">
+                            <Home/>
+                        </Route>
+                        <Route path="/about" exact="exact">
+                            <About/>
+                        </Route>
+                        <Route path="/service" exact="exact">
+                            <Services/>
+                        </Route>
+                        <Route path="/testimonial" exact="exact">
+                            <Testimonial/>
+                        </Route>
+                        <Route path="/contact" exact="exact">
+                            <Contact/>
+                        </Route>
+                        <Redirect to="/"/>
+                    </Switch>
+                </main>
+                </Langcontext.Provider>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
